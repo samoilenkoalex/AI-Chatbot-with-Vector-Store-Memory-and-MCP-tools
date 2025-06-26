d59c1d19a09d1dbfa39aaa6e34f27647eda18044
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2025-06-26 17:34:24 +0300

    add tailwind

---
+++ backend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./public/**/*.{html,js}', './src/**/*.{js,ts}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
